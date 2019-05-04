51f5cff54dbc158ea77a773aa3ce057bfd51632a
shafali03 <shaf03sm@gmail.com>
2019-05-04 19:43:17 +0100

    setup

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// Select element
+
+const loading = document.querySelector('.loading');
+const searchForm = document.getElementById('.searchForm');
+const output = document.querySelector('.output');
+const search = document.getElementById('search');
+const feedback = document.querySelector('.feedback');
+
+
